a2aafcab94456acc18fbec27ece6e2df6454793b
Ardiyan Agus <iyan@sgo.co.id>
2024-07-22 08:23:23 +0700

    install bootstrap

---
+++ src/components/Layouts/Navbar.jsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+  return (
+    <div className="d-flex justify-content-around">
+      <p>Download Tokopedia App</p>
+
+      <ul className="d-flex list-inline">
+        <li>Tentang Tokopedia</li>
+        <li>Tentang Tokopedia</li>
+        <li>Tentang Tokopedia</li>
+        <li>Tentang Tokopedia</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/home.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Layouts/Navbar";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      {/* <h1>Home Page</h1> */}
+    </>
+  );
+};
+
+export default Home;
