ba5fdeca9b0076bdf5c8cee3b5807127bdbb749e
Ardiyan Agus <iyan@sgo.co.id>
2024-07-23 21:13:43 +0700

    update header styling

--- src/components/Fragments/Header.jsx
+++ src/components/Fragments/Header.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
 import "../../styles/components/header.css";
 import { Icon } from "../Elements/Icon/Icon";
 
 const Header = () => {
+  const [isFocused, setIsFocused] = useState(false);
+
+  const handleFocus = () => {
+    setIsFocused(true);
+  };
+
+  const handleBluer = () => {
+    setIsFocused(false);
+  };
+
   return (
     <div className="d-flex">
       <img
@@ -14,14 +25,19 @@ const Header = () => {
       <p id="category">Kategori</p>
       {/* <div className="flex"> */}
       <div className="d-flex justify-content-center w-100">
-        <div className="d-flex justify-content-center align-self-center border-icon">
+        <div
+          id={`${isFocused ? "border-focus" : ""}`}
+          className="d-flex justify-content-center align-self-center border-icon"
+        >
           <Icon className="align-self-center" iconName="Search" size={20} />
         </div>
         <input
-            id="search-form-1"
+          id="search-form-1"
           type="text"
-          className="w-100 form-control search-form"
+          className="w-100 form-control search-form shadow-none"
           placeholder="Cari di Tokopedia"
+          onFocus={handleFocus}
+          onBlur={handleBluer}
         />
       </div>
       {/* </div> */}
