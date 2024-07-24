09ec3a5b9e33ddff4b05b661e5c73dcd1adbcb14
Ardiyan Agus <iyan@sgo.co.id>
2024-07-24 08:05:35 +0700

    create button header

--- src/components/Fragments/Header.jsx
+++ src/components/Fragments/Header.jsx
@@ -6,21 +6,15 @@ const Header = () => {
   const [isFocused, setIsFocused] = useState(false);
 
   const focusHandler = () => {
-    setIsFocused(prevState => !prevState)
+    setIsFocused((prevState) => !prevState);
   };
 
   return (
     <div className="d-flex">
-      <img
-        src="/images/logo_tokped.svg"
-        alt="tokopedia-logo"
-        id="img-tokped"
-      />
+      <img src="/images/logo_tokped.svg" alt="tokopedia-logo" id="img-tokped" />
       <i className="bi bi-airplane-engines-fill"></i>
-
       <p id="category">Kategori</p>
-      {/* <div className="flex"> */}
-      <div className="d-flex justify-content-center w-100">
+      <div className="d-flex w-100">
         <div
           id={`${isFocused ? "border-focus" : ""}`}
           className="d-flex justify-content-center align-self-center border-icon"
@@ -28,15 +22,27 @@ const Header = () => {
           <Icon className="align-self-center" iconName="Search" size={20} />
         </div>
         <input
-          id="search-form-1"
+          id="search-form"
           type="text"
           className="w-100 form-control search-form shadow-none"
           placeholder="Cari di Tokopedia"
           onFocus={focusHandler}
           onBlur={focusHandler}
         />
+        <div className="cart-container d-flex justify-content-center">
+          <Icon className="align-self-center" iconName="Cart" size={25} />
+        </div>
+        <div className="button-container">
+          <div className="row">
+            <div className="col-md-6">
+              <button className="btn btn-secondary">Masuk</button>
+            </div>
+            <div className="col-md-6">
+              <button className="btn btn-primary">Daftar</button>
+            </div>
+          </div>
+        </div>
       </div>
-      {/* </div> */}
     </div>
   );
 };
