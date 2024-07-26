62a292d1d4275fbc137c40d23c72c670b105b0cd
Ardiyan Agus <iyan@sgo.co.id>
2024-07-26 08:12:28 +0700

    update caraousel style

--- src/components/Fragments/Banner.jsx
+++ src/components/Fragments/Banner.jsx
@@ -4,21 +4,28 @@ import Carousel from "react-bootstrap/Carousel";
 const Banner = () => {
   return (
     <Fragment>
-      <div className="container">
-        {/* <img src="/images/banner1.webp" alt="" />
+      {/* <img src="/images/banner1.webp" alt="" />
         <h1>Banner Section</h1> */}
-        <Carousel>
-          <Carousel.Item interval={1000}>
-            <img src="/images/banner1.webp" alt="banner1" />
-          </Carousel.Item>
-          <Carousel.Item interval={500}>
-            <img src="/images/banner2.webp" alt="banner2" />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img src="/images/banner3.webp" alt="banner3" />
-          </Carousel.Item>
-        </Carousel>
-      </div>
+        {/* <div className="mr-5 ml-5"> */}
+
+{/* <div className="row"> */}
+      <Carousel style={{ marginLeft: "5%", marginRight: "5%", borderRadius: "30px" }}>
+        <Carousel.Item interval={1000}>
+          <img
+            src="/images/banner1.webp"
+            alt="banner1"
+            style={{ width: "100%" }}
+          />
+        </Carousel.Item>
+        <Carousel.Item interval={500}>
+          <img src="/images/banner2.webp" alt="banner2" style={{ width: "100%" }}/>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src="/images/banner3.webp" alt="banner3" style={{ width: "100%" }}/>
+        </Carousel.Item>
+      </Carousel>
+            {/* </div> */}
+            {/* </div> */}
     </Fragment>
   );
 };
