fb71db280396cf6968abfd63f09ee899fff1d51a
Ardiyan Agus <iyan@sgo.co.id>
2024-07-30 08:12:08 +0700

    update images

--- src/components/Fragments/CardCategory.jsx
+++ src/components/Fragments/CardCategory.jsx
@@ -1,14 +1,47 @@
-import Card from 'react-bootstrap/Card';
+import Card from "react-bootstrap/Card";
 
 const CardCategory = () => {
   return (
-    <div className="d-flex w-100 mt-4">
-      <Card className='shadow'>
+    <div className="w-100 mt-4">
+      <Card className="shadow">
         <Card.Body>
-          <Card.Title><h3 className='font-weight-bold'>Kategori Pilihan</h3></Card.Title>
+          <div className="row">
+            <div className="col-md-6">
+              <Card.Title>
+                <h4 className="font-weight-bold">Kategori Pilihan</h4>
+                <div className="d-flex justify-content-between" style={{ zIndex: '-10' }}>
+                  <div style={{ border: "1px solid black", borderRadius: '15px' }}>
+                    <img src="/images/product1.webp" alt="" style={{ borderRadius: "inherit" }}/>
+                  </div>
+                  <div style={{ border: "1px solid black", borderRadius: '15px' }}>
+                    <img src="/images/product1.webp" alt="" style={{ borderRadius: "inherit" }}/>
+                  </div>
+                  <div style={{ border: "1px solid black", borderRadius: '15px' }}>
+                    <img src="/images/product1.webp" alt="" style={{ borderRadius: "inherit" }}/>
+                  </div>
+                  <div style={{ border: "1px solid black", borderRadius: '15px' }}>
+                    <img src="/images/product1.webp" alt="" style={{ borderRadius: "inherit" }}/>
+                  </div>
+                  <div style={{ border: "1px solid black", borderRadius: '15px' }}>
+                    <img src="/images/product1.webp" alt="" style={{ borderRadius: "inherit" }}/>
+                  </div>
+                  <div style={{ border: "1px solid black", borderRadius: '15px' }}>
+                    <img src="/images/product1.webp" alt="" style={{ borderRadius: "inherit" }}/>
+                  </div>
+                </div>
+              </Card.Title>
+            </div>
+            <div className="col-md-6">
+              <Card.Title>
+                <h4 className="font-weight-bold">Top Up & Tagihan</h4>
+                <div className="d-flex justify-content-center">
+                <h1>asdds</h1>
+                </div>
+              </Card.Title>
+            </div>
+          </div>
           <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+            test
           </Card.Text>
         </Card.Body>
       </Card>
